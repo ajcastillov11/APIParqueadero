ce8d6fe79a6e3e96e7d47ba05adf8617363c48ce
Andres Javier Castillo Varela <ajvarela@latam.stefanini.com>
2023-05-15 11:51:27 -0500

    UPDATE

--- APIParqueadero/Controllers/EstacionamientoController.cs
+++ APIParqueadero/Controllers/EstacionamientoController.cs
@@ -4,65 +4,65 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace APIParqueadero.Api.Controllers
 {
-	[Route("api/[controller]")]
-	[ApiController]
-	public class EstacionamientoController : ControllerBase
-	{
-		private readonly IEstacionamientoService _estacionamientoService;
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EstacionamientoController : ControllerBase
+    {
+        private readonly IEstacionamientoService _estacionamientoService;
 
-		public EstacionamientoController(IEstacionamientoService vehiculoService)
-		{
-			_estacionamientoService = vehiculoService;
-		}
+        public EstacionamientoController(IEstacionamientoService vehiculoService)
+        {
+            _estacionamientoService = vehiculoService;
+        }
 
-		[HttpGet("ListadoVehiculos")]
-		public async Task<ActionResult<List<VehiculosDto>>> GetVehiculos(DateTime fechaInicial, DateTime fechaFinal)
-		{
-			try
-			{
-				List<VehiculosDto> listaVehiculos = await _estacionamientoService.GetListadoVehiculos(fechaInicial, fechaFinal);
-				return Ok(listaVehiculos);
-			}
-			catch (Exception ex)
-			{
+        [HttpGet("ListadoVehiculos/{fechaInicial}/{fechaFinal}")]
+        public async Task<ActionResult<List<VehiculosDto>>> GetVehiculos(DateTime fechaInicial, DateTime fechaFinal)
+        {
+            try
+            {
+                List<VehiculosDto> listaVehiculos = await _estacionamientoService.GetListadoVehiculos(fechaInicial, fechaFinal);
+                return Ok(listaVehiculos);
+            }
+            catch (Exception ex)
+            {
 
-				throw new Exception(ex.Message);
-			}
-		}
+                throw new Exception(ex.Message);
+            }
+        }
 
-		[HttpPut("Liquidar")]
-		public async Task<ActionResult<string>> LiquidarEstacionamiento(LiquidacionDto liquidacionDto)
-		{
-			try
-			{
-				if (liquidacionDto is null)
-				{
-					return Problem("El request no puede ser vacio.");
-				}
+        [HttpPut("Liquidar")]
+        public async Task<ActionResult<string>> LiquidarEstacionamiento(LiquidacionDto liquidacionDto)
+        {
+            try
+            {
+                if (liquidacionDto is null)
+                {
+                    return Problem("El request no puede ser vacio.");
+                }
 
-				Models.Vehiculo? vehiculo = await _estacionamientoService.VehiculoExistente(liquidacionDto.Placa);
-				if (vehiculo == null)
-				{
-					return NotFound($"El Vehículo con placas {liquidacionDto.Placa} no se encuentra registrado en este paqueadero");
-				}
+                Models.Vehiculo? vehiculo = await _estacionamientoService.VehiculoExistente(liquidacionDto.Placa);
+                if (vehiculo == null)
+                {
+                    return NotFound($"El Vehículo con placas {liquidacionDto.Placa} no se encuentra registrado en este paqueadero");
+                }
 
-				return Ok(await _estacionamientoService.LiquidarEstacionamiento(liquidacionDto, vehiculo));
-			}
-			catch (Exception ex)
-			{
-				throw new Exception(ex.Message);
-			}
-		}
+                return Ok(await _estacionamientoService.LiquidarEstacionamiento(liquidacionDto, vehiculo));
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
 
-		[HttpPost("RegistrarIngreso")]
-		public async Task<ActionResult<string>> RegistrarIngreso(VehiculoDto vehiculo)
-		{
-			if (vehiculo is null)
-			{
-				return Problem("El request no puede ser vacio.");
-			}
+        [HttpPost("RegistrarIngreso")]
+        public async Task<ActionResult<string>> RegistrarIngreso(VehiculoDto vehiculo)
+        {
+            if (vehiculo is null)
+            {
+                return Problem("El request no puede ser vacio.");
+            }
 
-			return Ok(await _estacionamientoService.RegistrarIngreso(vehiculo));
-		}
-	}
+            return Ok(await _estacionamientoService.RegistrarIngreso(vehiculo));
+        }
+    }
 }
